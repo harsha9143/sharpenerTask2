325a8e3b39a0701b91c6bd87f720bb55e005e805
harsha9143 <harshavardhan9143@gmail.com>
2025-09-03 12:01:45 +0530

    fifth commit

--- index.js
+++ index.js
@@ -31,3 +31,47 @@ fruitsList[4].style.color = "blue";
 for (let i = 0; i < fruitsList.length; i++) {
   fruitsList[i].style.fontStyle = "italic";
 }
+
+// Write the code as shown in the video below:
+
+const mainHeading = document.querySelector("#main-heading");
+mainHeading.style.textAlign = "center";
+
+const fruits = document.querySelector(".fruits");
+fruits.style.backgroundColor = "gray";
+fruits.style.padding = "30px";
+fruits.style.margin = "30px";
+fruits.style.width = "50%";
+fruits.style.borderRadius = "5px";
+fruits.style.listStyleType = "none";
+
+const basketHeading = document.querySelector("h2");
+basketHeading.style.marginLeft = "30px";
+
+const fruitsList = document.querySelectorAll(".fruit");
+
+fruitsList.forEach((fruit) => {
+  fruit.style.backgroundColor = "white";
+  fruit.style.padding = "10px";
+  fruit.style.margin = "10px";
+  fruit.style.borderRadius = "5px";
+});
+
+const oddFruitsList = document.querySelectorAll(".fruit:nth-child(odd)");
+
+oddFruitsList.forEach((fruit) => {
+  fruit.style.backgroundColor = "lightgray";
+});
+
+// Write answer to the questions asked below:
+
+const basketHeading2 = document.querySelector("#basket-heading");
+
+basketHeading2.style.color = "brown";
+
+const evenFruitsList = document.querySelectorAll(".fruit:nth-child(even)");
+
+evenFruitsList.forEach((fruit) => {
+  fruit.style.color = "white";
+  fruit.style.backgroundColor = "red";
+});
